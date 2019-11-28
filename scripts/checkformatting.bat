a11c6c31a6bdf4138fd4dbfc9d8b95f709984841
Marcus Hirt <marcus@hirt.se>
2019-11-29 00:52:48 +0100

    Cleanup

--- scripts/checkformatting.bat
+++ scripts/checkformatting.bat
@@ -3,8 +3,8 @@ echo "======== Building p2 repo ==================="
 cd releng\third-party
 mvn p2:site
 start /B cmd /C "mvn jetty:run"
-echo "======== Entering core ======================"
 cd ..\..\core
+echo "======== Installing core ===================="
 call mvn install
 echo "======== Running spotless for core =========="
 mvn spotless:check
