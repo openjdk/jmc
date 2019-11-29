e2d8a1d7d10c7e7b75c94ada9a145d42f6e14b5c
Marcus Hirt <marcus@hirt.se>
2019-11-29 01:35:05 +0100

    Windows and I are not friends

--- scripts/runapptests.bat
+++ scripts/runapptests.bat
@@ -1,11 +1,13 @@
 @echo off
-REM Seems like the p2 repo needs to be started again...
-REM Note that it is built as part of the formatting check, so no need 
-REM to rebuild
+REM Seems we need to restart repo
 cd releng\third-party
 echo "======== Starting p2 repo ==================="
 start /B cmd /C "mvn jetty:run"
-cd ..\..
+cd ..\..\core
+REM Seems we need to re-install core
+echo "======== Installing core ===================="
+call mvn install
+cd ..
 echo "======== Running application tests =========="
 mvn verify
 echo "======== Finished ==========================="
