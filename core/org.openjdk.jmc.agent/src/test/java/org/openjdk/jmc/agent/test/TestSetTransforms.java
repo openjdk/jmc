67bc8668b74fe15d0072e60115068c2e32fcb837
Jessye Coleman-Shapiro <jescolem@redhat.com>
2020-01-10 15:59:20 +0000

    6658: Remove unnecessary storing of pre-instrumented bytecode
    Reviewed-by: hirt

--- core/org.openjdk.jmc.agent/src/test/java/org/openjdk/jmc/agent/test/TestSetTransforms.java
+++ core/org.openjdk.jmc.agent/src/test/java/org/openjdk/jmc/agent/test/TestSetTransforms.java
@@ -33,37 +33,53 @@
  */
 package org.openjdk.jmc.agent.test;
 
-import static org.junit.Assert.assertNotNull;
-
-import java.io.PrintWriter;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
 import java.lang.management.ManagementFactory;
-import java.util.logging.Level;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 
 import javax.management.MBeanServer;
 import javax.management.ObjectName;
 
 import org.junit.Test;
 import org.objectweb.asm.ClassReader;
-import org.objectweb.asm.util.CheckClassAdapter;
-import org.objectweb.asm.util.TraceClassVisitor;
-import org.openjdk.jmc.agent.Agent;
-import org.openjdk.jmc.agent.test.util.TestToolkit;
+import org.objectweb.asm.ClassVisitor;
+import org.objectweb.asm.ClassWriter;
+import org.objectweb.asm.MethodVisitor;
+import org.objectweb.asm.Opcodes;
+import org.objectweb.asm.commons.AdviceAdapter;
+import org.openjdk.jmc.agent.Method;
+import org.openjdk.jmc.agent.Parameter;
+import org.openjdk.jmc.agent.ReturnValue;
+import org.openjdk.jmc.agent.jfr.JFRTransformDescriptor;
+import org.openjdk.jmc.agent.jfrnext.impl.JFRNextEventClassGenerator;
+import org.openjdk.jmc.agent.util.TypeUtils;
 
 public class TestSetTransforms {
 
 	private static final String AGENT_OBJECT_NAME = "org.openjdk.jmc.jfr.agent:type=AgentController"; //$NON-NLS-1$
+	private static final String EVENT_ID = "demo.jfr.test6";
+	private static final String EVENT_NAME = "JFR Hello World Event 6 %TEST_NAME%";
+	private static final String EVENT_DESCRIPTION = "JFR Hello World Event 6 %TEST_NAME%";
+	private static final String EVENT_PATH = "demo/jfrhelloworldevent6";
+	private static final String EVENT_CLASS_NAME = "org.openjdk.jmc.agent.test.InstrumentMe";
+	private static final String METHOD_NAME = "printHelloWorldJFR6";
+	private static final String METHOD_DESCRIPTOR = "()D";
 
 	private static final String XML_DESCRIPTION = "<jfragent>"
 			+ "<events>"
-			+ "<event id=\"demo.jfr.test1\">"
-			+ "<name>JFR Hello World Event 1 %TEST_NAME%</name>"
-			+ "<description>Defined in the xml file and added by the agent.</description>"
-			+ "<path>demo/jfrhelloworldevent1</path>"
+			+ "<event id=\"" + EVENT_ID + "\">"
+			+ "<name>" + EVENT_NAME + "</name>"
+			+ "<description>" + EVENT_DESCRIPTION + "</description>"
+			+ "<path>" + EVENT_PATH + "</path>"
 			+ "<stacktrace>true</stacktrace>"
-			+ "<class>org.openjdk.jmc.agent.test.InstrumentMe</class>"
+			+ "<class>" + EVENT_CLASS_NAME + "</class>"
 			+ "<method>"
-			+ "<name>printHelloWorldJFR1</name>"
-			+ "<descriptor>()V</descriptor>"
+			+ "<name>" + METHOD_NAME + "</name>"
+			+ "<descriptor>" + METHOD_DESCRIPTOR + "</descriptor>"
 			+ "</method>"
 			+ "<location>WRAP</location>"
 			+ "</event>"
@@ -72,40 +88,84 @@ public class TestSetTransforms {
 
 	@Test
 	public void testSetTransforms() throws Exception {
-		// Invoke retransform
-		MBeanServer mbs = ManagementFactory.getPlatformMBeanServer();
-		ObjectName name = new ObjectName(AGENT_OBJECT_NAME);
-		Object[] parameters = {XML_DESCRIPTION};
-		String[] signature = {String.class.getName()};
-		Class<?>[] clazzes = (Class<?>[]) mbs.invoke(name, "setTransforms", parameters, signature);
-		assertNotNull(clazzes);
-		if (Agent.getLogger().isLoggable(Level.FINE)) {
-			for (Class<?> clazz : clazzes) {
-				// If we've asked for verbose information, we write the generated class
-				TraceClassVisitor visitor = new TraceClassVisitor(new PrintWriter(System.out));
-				new CheckClassAdapter(visitor);
-				new ClassReader(TestToolkit.getByteCode(clazz));
-			}
+		boolean exceptionThrown = false;
+		try {
+			InstrumentMe.printHelloWorldJFR6();
+		} catch (Exception e) {
+			e.printStackTrace(System.err);
+			exceptionThrown = true;
 		}
+		assertFalse(exceptionThrown);
+
+		injectFailingEvent();
+		doSetTransforms(XML_DESCRIPTION);
+		try {
+			InstrumentMe.printHelloWorldJFR6();
+		} catch (RuntimeException e) {
+			exceptionThrown = true;
+		}
+		assertTrue(exceptionThrown);
+
+		doSetTransforms("");
+		try {
+			InstrumentMe.printHelloWorldJFR6();
+			exceptionThrown = false;
+		} catch (Exception e) {
+			e.printStackTrace(System.err);
+		}
+		assertFalse(exceptionThrown);
 	}
 
-	@Test
-	public void testClearAllTransforms() throws Exception {
-		// Invoke retransform
-		MBeanServer mbs = ManagementFactory.getPlatformMBeanServer();
-		ObjectName name = new ObjectName(AGENT_OBJECT_NAME);
-		Object[] parameters = {""};
-		String[] signature = {String.class.getName()};
-		Class<?>[] clazzes = (Class<?>[]) mbs.invoke(name, "setTransforms", parameters, signature);
-		assertNotNull(clazzes);
-		if (Agent.getLogger().isLoggable(Level.FINE)) {
-			for (Class<?> clazz : clazzes) {
-				// If we've asked for verbose information, we write the generated class
-				TraceClassVisitor visitor = new TraceClassVisitor(new PrintWriter(System.out));
-				new CheckClassAdapter(visitor);
-				new ClassReader(TestToolkit.getByteCode(clazz));
+	private void injectFailingEvent() throws Exception {
+		Method method = new Method(METHOD_NAME, METHOD_DESCRIPTOR);
+		Map<String, String> attributes = new HashMap<String, String>();
+		attributes.put("path", EVENT_PATH);
+		attributes.put("name", EVENT_NAME);
+		attributes.put("description", EVENT_DESCRIPTION);
+		ReturnValue retVal = new ReturnValue(null, "", null);
+		JFRTransformDescriptor eventTd = new JFRTransformDescriptor(EVENT_ID,
+				EVENT_CLASS_NAME.replace(".", "/"), method, attributes, new ArrayList<Parameter>(), retVal);
+
+		ClassWriter classWriter = new ClassWriter(ClassWriter.COMPUTE_MAXS);
+		ClassVisitor classVisitor = new ClassVisitor(Opcodes.ASM5, classWriter) {
+			@Override
+			public MethodVisitor visitMethod(int access, String name, String desc, String signature,
+					String[] exceptions) {
+				MethodVisitor mv = super.visitMethod(access, name, desc, signature, exceptions);
+				if (!name.equals("<init>")) {
+					return mv;
+				}
+				return new AdviceAdapter(Opcodes.ASM5, mv, access, name, "()V") {
+					@Override
+					protected void onMethodExit(int opcode) {
+						mv.visitTypeInsn(Opcodes.NEW, "java/lang/RuntimeException");
+						mv.visitInsn(Opcodes.DUP);
+						mv.visitMethodInsn(Opcodes.INVOKESPECIAL, "java/lang/RuntimeException", "<init>", "()V", false); //$NON-NLS-1$ //$NON-NLS-2$
+						mv.visitInsn(Opcodes.ATHROW);
+
+						mv.visitFrame(F_NEW, 0, new Object[0], 0, new Object[0]);
+						mv.visitInsn(Opcodes.ACONST_NULL);
+					}
+				};
 			}
-		}
+		};
+
+		byte[] eventClass = JFRNextEventClassGenerator.generateEventClass(eventTd);
+		ClassReader reader = new ClassReader(eventClass);
+		reader.accept(classVisitor, 0);
+		byte[] modifiedEvent = classWriter.toByteArray();
+
+		TypeUtils.defineClass(eventTd.getEventClassName(), modifiedEvent, 0, modifiedEvent.length,
+				ClassLoader.getSystemClassLoader(), null);
+	}
+
+	private void doSetTransforms(String xmlDescription) throws Exception  {
+		ObjectName name = new ObjectName(AGENT_OBJECT_NAME);
+		Object[] parameters = {xmlDescription};
+		String[] signature = {String.class.getName()};
+
+		MBeanServer mbs = ManagementFactory.getPlatformMBeanServer();
+		mbs.invoke(name, "setTransforms", parameters, signature);
 	}
 
 	public void test() {
