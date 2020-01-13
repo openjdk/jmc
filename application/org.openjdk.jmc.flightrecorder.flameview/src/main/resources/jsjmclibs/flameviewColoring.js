59afc3d9acc0a0aae164b5f70ac6140404a71f54
Miro Wengner <miro.wengner@gmail.com>
2020-01-13 20:47:39 +0100

    6549: Color flame chart based on package name - java script file formatting

--- application/org.openjdk.jmc.flightrecorder.flameview/src/main/resources/jsjmclibs/flameviewColoring.js
+++ application/org.openjdk.jmc.flightrecorder.flameview/src/main/resources/jsjmclibs/flameviewColoring.js
@@ -36,8 +36,8 @@ const colorByPackage = function (p) {
     if (p === undefined) {
         return invalidPackageColor;
     } else {
-    	const packageNameStrip = stripPackageName(p);	
-        const packageSelectedColor = packageColorMap.get(packageNameStrip);
+    	const packageNameStrip = stripPackageName(p);
+    	const packageSelectedColor = packageColorMap.get(packageNameStrip);
         if (packageSelectedColor === undefined) {
         	const packageMarkerSelected = getPackageMarker(packageNameStrip);
         	const packageNameStripHash = packageNameStrip.hashCode();
