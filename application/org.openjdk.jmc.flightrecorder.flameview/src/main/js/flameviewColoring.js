c2134e2fad607e194e88f8f7dbfe1039725058ce
Marcus Hirt <hirt@openjdk.org>
2020-01-20 12:53:32 +0000

    6672: JavaScript formatting template
    Reviewed-by: hdafgard

--- application/org.openjdk.jmc.flightrecorder.flameview/src/main/js/flameviewColoring.js
+++ application/org.openjdk.jmc.flightrecorder.flameview/src/main/js/flameviewColoring.js
@@ -1,24 +1,24 @@
 /*
  Copyright (c) 2020, Oracle and/or its affiliates. All rights reserved.
  Copyright (c) 2020, Datadog, Inc. All rights reserved.
- 
+
  DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
- 
+
  The contents of this file are subject to the terms of either the Universal Permissive License
  v 1.0 as shown at http://oss.oracle.com/licenses/upl
- 
+
  or the following license:
- 
+
  Redistribution and use in source and binary forms, with or without modification, are permitted
  provided that the following conditions are met:
- 
+
  1. Redistributions of source code must retain the above copyright notice, this list of conditions
  and the following disclaimer.
- 
+
  2. Redistributions in binary form must reproduce the above copyright notice, this list of
  conditions and the following disclaimer in the documentation and/or other materials provided with
  the distribution.
- 
+
  3. Neither the name of the copyright holder nor the names of its contributors may be used to
  endorse or promote products derived from this software without specific prior written permission.
 
@@ -33,14 +33,14 @@
  */
 
 String.prototype.hashCode = function () {
-    var hash = 0;
-    if (this.length === 0) return hash;
-    for (var i = 0; i < this.length; i++) {
-        var char = this.charCodeAt(i);
-        hash = ((hash << 5) - hash) + char;
-        hash = hash & hash;
-    }
-    return hash;
+	var hash = 0;
+	if (this.length === 0) return hash;
+	for (var i = 0; i < this.length; i++) {
+		var char = this.charCodeAt(i);
+		hash = ((hash << 5) - hash) + char;
+		hash = hash & hash;
+	}
+	return hash;
 };
 
 const rootPackageColor = "darkred";
@@ -56,54 +56,54 @@ const packageMarkerJava = "java";
 const packageMarkerSun = "sun";
 const packageMarkerComSunAndJdk = "comSunAndJdk";
 const packageMarkerRest = "rest";
-const packagesIdentifierMap = new Map().set("java.", packageMarkerJava).set("sun.", packageMarkerSun)
-    .set("com.sun.", packageMarkerComSunAndJdk).set("jdk.", packageMarkerComSunAndJdk);
+const packagesIdentifierMap = new Map().set("java.", packageMarkerJava).set("sun.", packageMarkerSun).set("com.sun.", 
+		packageMarkerComSunAndJdk).set("jdk.", packageMarkerComSunAndJdk);
 const packageColorMap = new Map().set("", rootPackageColor);
 
 const colorByPackage = function (p) {
-    if (p === undefined) {
-        return invalidPackageColor;
-    } else {
-        const packageNameStrip = stripPackageName(p);
-        const packageSelectedColor = packageColorMap.get(packageNameStrip);
-        if (packageSelectedColor === undefined) {
-            const packageMarkerSelected = getPackageMarker(packageNameStrip);
-            const packageNameStripHash = packageNameStrip.hashCode();
-            switch (packageMarkerSelected) {
-                case packageMarkerJava:
-                    packageColorMap.set(packageNameStrip, packageJavaColorLightGray);
-                    break;
-                case packageMarkerComSunAndJdk:
-                    packageColorMap.set(packageNameStrip, packageComSunAndJdkColorDarkGray);
-                    break;
-                case packageMarkerSun:
-                    packageColorMap.set(packageNameStrip, packageSunDarkColorGray);
-                    break;
-                case packageMarkerRest:
-                    const packageRestSelectedColor = createHslColorString(adjustHslPropertyByHash(packageNameStripHash, packageRestValueH, packageRestValueHMax), packageRestSLValues[0], packageRestSLValues[1]);
-                    packageColorMap.set(packageNameStrip, packageRestSelectedColor);
-                    break;
-            }
-            return packageColorMap.get(packageNameStrip);
-        } else {
-            return packageSelectedColor;
-        }
-    }
+	if (p === undefined) {
+		return invalidPackageColor;
+	} else {
+		const packageNameStrip = stripPackageName(p);
+		const packageSelectedColor = packageColorMap.get(packageNameStrip);
+		if (packageSelectedColor === undefined) {
+			const packageMarkerSelected = getPackageMarker(packageNameStrip);
+			const packageNameStripHash = packageNameStrip.hashCode();
+			switch (packageMarkerSelected) {
+			case packageMarkerJava:
+				packageColorMap.set(packageNameStrip, packageJavaColorLightGray);
+				break;
+			case packageMarkerComSunAndJdk:
+				packageColorMap.set(packageNameStrip, packageComSunAndJdkColorDarkGray);
+				break;
+			case packageMarkerSun:
+				packageColorMap.set(packageNameStrip, packageSunDarkColorGray);
+				break;
+			case packageMarkerRest:
+				const packageRestSelectedColor = createHslColorString(adjustHslPropertyByHash(packageNameStripHash, packageRestValueH, packageRestValueHMax), packageRestSLValues[0], packageRestSLValues[1]);
+				packageColorMap.set(packageNameStrip, packageRestSelectedColor);
+				break;
+			}
+			return packageColorMap.get(packageNameStrip);
+		} else {
+			return packageSelectedColor;
+		}
+	}
 };
 
 const getPackageMarker = function(p){
-    for(let k of packagesIdentifierMap.keys()){
-        if(p.startsWith(k)){
-            return packagesIdentifierMap.get(k);
-        }
-    }
-    return packageMarkerRest;
+	for(let k of packagesIdentifierMap.keys()){
+		if(p.startsWith(k)){
+			return packagesIdentifierMap.get(k);
+		}
+	}
+	return packageMarkerRest;
 };
 
 const stripPackageName = function (p) {
-    const splitString = p.split("\u002E");
-    const number = Math.min(splitString.length, packageConsideredDepth);
-    return splitString.slice(0, number).join("\u002E");
+	const splitString = p.split("\u002E");
+	const number = Math.min(splitString.length, packageConsideredDepth);
+	return splitString.slice(0, number).join("\u002E");
 };
 
 const adjustHslPropertyByHash = function (hash, min, max) {
@@ -112,11 +112,11 @@ const adjustHslPropertyByHash = function (hash, min, max) {
 };
 
 const createHslColorString = function(h,s,l){
-    return "hsl\u0028" + h + "\u002c " + s + "\u0025\u002c " + l + "\u0025\u0029";
+	return "hsl\u0028" + h + "\u002c " + s + "\u0025\u002c " + l + "\u0025\u0029";
 };
 
 const colorCell = function (d) {
-    return colorByPackage(d.data.p);
+	return colorByPackage(d.data.p);
 };
 
 const adjustTip = function (d) {
