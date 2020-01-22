9a5d526dab366b0c77cf066908aa0558da233bcd
Miro Wengner <miro.wengner@gmail.com>
2020-01-22 18:17:57 +0100

    6670: message bundle correction

--- core/org.openjdk.jmc.flightrecorder/src/main/java/org/openjdk/jmc/flightrecorder/stacktrace/messages/common/Messages.java
+++ core/org.openjdk.jmc.flightrecorder/src/main/java/org/openjdk/jmc/flightrecorder/stacktrace/messages/common/Messages.java
@@ -37,7 +37,7 @@ import java.util.MissingResourceException;
 import java.util.ResourceBundle;
 
 public class Messages {
-	private static final String BUNDLE_NAME = "org.openjdk.jmc.flightrecorder.stacktrace.messages.common"; //$NON-NLS-1$
+	private static final String BUNDLE_NAME = "org.openjdk.jmc.flightrecorder.stacktrace.messages.common.messages"; //$NON-NLS-1$
 
 	private static final ResourceBundle RESOURCE_BUNDLE = ResourceBundle.getBundle(BUNDLE_NAME);
 
