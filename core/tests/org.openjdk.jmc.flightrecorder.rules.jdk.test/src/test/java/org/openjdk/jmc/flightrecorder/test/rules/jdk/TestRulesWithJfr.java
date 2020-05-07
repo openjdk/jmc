9036f22b7093067dc19533384f8570311700759d
Marcus Hirt <hirt@openjdk.org>
2020-05-07 13:37:29 +0000

    6778: Removing dependency on OPS4j maven repo and javax.mail:dsn
    Co-authored-by: Jason Zaugg <jzaugg@gmail.com>
    Reviewed-by: jkang, hdafgard, ghb

--- core/tests/org.openjdk.jmc.flightrecorder.rules.jdk.test/src/test/java/org/openjdk/jmc/flightrecorder/test/rules/jdk/TestRulesWithJfr.java
+++ core/tests/org.openjdk.jmc.flightrecorder.rules.jdk.test/src/test/java/org/openjdk/jmc/flightrecorder/test/rules/jdk/TestRulesWithJfr.java
@@ -33,7 +33,6 @@
 package org.openjdk.jmc.flightrecorder.test.rules.jdk;
 
 import java.io.File;
-import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.OutputStream;
