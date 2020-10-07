6d9317b9ddf706f8f19645041e25f237af172af1
Jean-Philippe Bempel <jean-philippe@bempel.fr>
2020-10-07 20:55:58 +0200

    6173: jmc -open requires full path
    Handle special Mac case where user.dir is set by mac eclipse launcher
    to the directory where is jmc binary.
    Hopfully, PWD env var is kept where the program was launched so we can use it
    to prepend to the providing filename.

--- application/org.openjdk.jmc.ui/src/main/java/org/openjdk/jmc/ui/MCPathEditorInput.java
+++ application/org.openjdk.jmc.ui/src/main/java/org/openjdk/jmc/ui/MCPathEditorInput.java
@@ -42,8 +42,9 @@ import org.eclipse.ui.IEditorInput;
 import org.eclipse.ui.IMemento;
 import org.eclipse.ui.IPathEditorInput;
 import org.eclipse.ui.IPersistableElement;
-
 import org.openjdk.jmc.ui.common.util.AdapterUtil;
+import org.openjdk.jmc.ui.common.util.Environment;
+import org.openjdk.jmc.ui.common.util.Environment.OSType;
 
 /**
  * Class for inputs that consists of a path
@@ -102,7 +103,19 @@ public class MCPathEditorInput implements IPathEditorInput, IPersistableElement 
 
 	@Override
 	public IPath getPath() {
-		return Path.fromOSString(m_file.getAbsolutePath());
+		String absPath = m_file.getAbsolutePath();
+		if (Environment.getOSType() == OSType.MAC) {
+			if (!m_file.isAbsolute()) {
+				String pwd = System.getenv().get("PWD");
+				if (pwd != null) {
+					File absFile = new File(pwd, m_file.getName());
+					if (absFile.exists()) {
+						absPath = absFile.getAbsolutePath();
+					}
+				}
+			}
+		}
+		return Path.fromOSString(absPath);
 	}
 
 	@Override
