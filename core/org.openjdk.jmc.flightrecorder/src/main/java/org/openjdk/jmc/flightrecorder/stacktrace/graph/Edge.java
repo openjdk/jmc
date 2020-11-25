205680f764c3261716aaa71483369cfc746573ac
Marcus Hirt <hirt@openjdk.org>
2020-11-25 11:34:30 +0000

    6507: Use GraphViz to render stack traces
    Reviewed-by: mwengner, ghb

---
+++ core/org.openjdk.jmc.flightrecorder/src/main/java/org/openjdk/jmc/flightrecorder/stacktrace/graph/Edge.java
@@ -0,0 +1,110 @@
+/*
+ * Copyright (c) 2019, 2020, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2019, 2020, Datadog, Inc. All rights reserved.
+ *
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
+ *
+ * The contents of this file are subject to the terms of either the Universal Permissive License
+ * v 1.0 as shown at http://oss.oracle.com/licenses/upl
+ *
+ * or the following license:
+ *
+ * Redistribution and use in source and binary forms, with or without modification, are permitted
+ * provided that the following conditions are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright notice, this list of conditions
+ * and the following disclaimer.
+ *
+ * 2. Redistributions in binary form must reproduce the above copyright notice, this list of
+ * conditions and the following disclaimer in the documentation and/or other materials provided with
+ * the distribution.
+ *
+ * 3. Neither the name of the copyright holder nor the names of its contributors may be used to
+ * endorse or promote products derived from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR
+ * IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND
+ * FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR
+ * CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
+ * WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY
+ * WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+package org.openjdk.jmc.flightrecorder.stacktrace.graph;
+
+/**
+ * An edge in the graph of aggregated stack traces.
+ */
+public final class Edge {
+	private final Node from;
+	private final Node to;
+	int count;
+	double value;
+
+	/**
+	 * Constructor.
+	 * 
+	 * @param from
+	 *            non null from node.
+	 * @param to
+	 *            non null to node.
+	 */
+	public Edge(Node from, Node to) {
+		if (from == null || to == null) {
+			throw new NullPointerException("Nodes must not be null");
+		}
+		this.from = from;
+		this.to = to;
+	}
+
+	public Node getFrom() {
+		return from;
+	}
+
+	public Node getTo() {
+		return to;
+	}
+
+	public int getCount() {
+		return count;
+	}
+
+	public double getValue() {
+		return value;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + from.hashCode();
+		result = prime * result + to.hashCode();
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj) {
+			return true;
+		}
+		if (obj == null) {
+			return false;
+		}
+		if (getClass() != obj.getClass())
+			return false;
+		Edge other = (Edge) obj;
+		if (!from.equals(other.from)) {
+			return false;
+		}
+		if (!to.equals(other.to)) {
+			return false;
+		}
+		return true;
+	}
+
+	@Override
+	public String toString() {
+		return getFrom().toString() + " -> " + getTo().toString() + ", count = " + count;
+	}
+}
