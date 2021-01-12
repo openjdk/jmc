b5a9536c4435d56335b9d43c0fe15d7d087a968e
ghb <guru.hb@oracle.com>
2021-01-12 22:35:17 +0530

    Removed hardcoded year value

--- scripts/updatecopyrightyear.sh
+++ scripts/updatecopyrightyear.sh
@@ -1,10 +1,13 @@
 #!/bin/sh -l
 
+START_YEAR=2018
 CURRENT_YEAR=$(date +'%Y')
+PREVIOUS_YEAR=$(date +%Y | awk -F '/' '{$NF--}1')
 
 echo "Current year : $CURRENT_YEAR"
+echo "START_YEAR : $START_YEAR"
+echo "PREVIOUS_YEAR : $PREVIOUS_YEAR"
 
-# Modified
 MODIFIED_FILES=$(git status | grep modified: | awk 'NF>1{print $NF}')
 
 for filesToUpdate in $MODIFIED_FILES
@@ -14,8 +17,17 @@ do
     	|| [[ "$filesToUpdate" == *.java ]] \
     	|| [[ "$filesToUpdate" == *.properties ]]
     then
-   	sed -i ''  "/Copyright (c)/s/2020/$CURRENT_YEAR/" $filesToUpdate
-    	sed -i ''  "/Copyright (c)/s/2018, Oracle/2018, $CURRENT_YEAR, Oracle/" $filesToUpdate
+      if [ "$(uname)" == "Darwin" ]; then
+        # For Mac OS sed inplace doesn't work without these
+        sed -i '' '/Copyright (c)/s/$PREVIOUS_YEAR/$CURRENT_YEAR/' $filesToUpdate
+        sed -i '' '/Copyright (c)/s/$START_YEAR, Oracle/$START_YEAR, $CURRENT_YEAR, Oracle/' $filesToUpdate
+        sed -i '' "/^copyright=/s/$PREVIOUS_YEAR/$CURRENT_YEAR/" $filesToUpdate
+      else
+        echo "Non Mac"
+        sed -i "/Copyright (c)/s/$PREVIOUS_YEAR/$CURRENT_YEAR/" $filesToUpdate
+        sed -i "/Copyright (c)/s/$START_YEAR, Oracle/$START_YEAR, $CURRENT_YEAR, Oracle/" $filesToUpdate
+        sed -i "/copyright=/s/$PREVIOUS_YEAR/$CURRENT_YEAR/" $fileToupdate
+      fi
   		echo "Updated : $filesToUpdate"
 	else
     		echo "UnSupported files files : $filesToUpdate"
