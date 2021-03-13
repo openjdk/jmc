ddac55b92ad4ee479aa4347a58492988b60438a7
Brice Dutheil <brice.dutheil@gmail.com>
2021-03-13 17:14:17 +0100

    Introduces GC flags in GC configuration page

--- application/org.openjdk.jmc.flightrecorder.ui/src/main/java/org/openjdk/jmc/flightrecorder/ui/pages/GCConfigurationPage.java
+++ application/org.openjdk.jmc.flightrecorder.ui/src/main/java/org/openjdk/jmc/flightrecorder/ui/pages/GCConfigurationPage.java
@@ -32,7 +32,12 @@
  */
 package org.openjdk.jmc.flightrecorder.ui.pages;
 
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.Set;
+
 import org.eclipse.jface.resource.ImageDescriptor;
+import org.eclipse.jface.viewers.ColumnViewer;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.custom.SashForm;
 import org.eclipse.swt.widgets.Composite;
@@ -41,9 +46,17 @@ import org.eclipse.ui.forms.widgets.FormToolkit;
 import org.eclipse.ui.forms.widgets.Section;
 
 import org.openjdk.jmc.common.IState;
+import org.openjdk.jmc.common.IWritableState;
+import org.openjdk.jmc.common.item.IAccessorFactory;
 import org.openjdk.jmc.common.item.IItemFilter;
+import org.openjdk.jmc.common.item.IMemberAccessor;
+import org.openjdk.jmc.common.item.IType;
+import org.openjdk.jmc.common.item.ItemFilters;
+import org.openjdk.jmc.flightrecorder.JfrAttributes;
 import org.openjdk.jmc.flightrecorder.jdk.JdkAggregators;
+import org.openjdk.jmc.flightrecorder.jdk.JdkAttributes;
 import org.openjdk.jmc.flightrecorder.jdk.JdkFilters;
+import org.openjdk.jmc.flightrecorder.jdk.JdkTypeIDs;
 import org.openjdk.jmc.flightrecorder.rules.util.JfrRuleTopics;
 import org.openjdk.jmc.flightrecorder.ui.FlightRecorderUI;
 import org.openjdk.jmc.flightrecorder.ui.IDataPageFactory;
@@ -54,11 +67,18 @@ import org.openjdk.jmc.flightrecorder.ui.IPageUI;
 import org.openjdk.jmc.flightrecorder.ui.StreamModel;
 import org.openjdk.jmc.flightrecorder.ui.common.AbstractDataPage;
 import org.openjdk.jmc.flightrecorder.ui.common.DataPageToolkit;
+import org.openjdk.jmc.flightrecorder.ui.common.FilterComponent;
 import org.openjdk.jmc.flightrecorder.ui.common.ImageConstants;
 import org.openjdk.jmc.flightrecorder.ui.common.ItemAggregateViewer;
+import org.openjdk.jmc.flightrecorder.ui.common.ItemHistogram;
+import org.openjdk.jmc.flightrecorder.ui.common.ItemHistogram.CompositeKeyHistogramBuilder;
 import org.openjdk.jmc.flightrecorder.ui.messages.internal.Messages;
 import org.openjdk.jmc.ui.accessibility.SimpleTraverseListener;
+import org.openjdk.jmc.ui.column.ColumnMenusFactory;
+import org.openjdk.jmc.ui.column.TableSettings;
+import org.openjdk.jmc.ui.handlers.MCContextMenuManager;
 import org.openjdk.jmc.ui.misc.CompositeToolkit;
+import org.openjdk.jmc.ui.misc.PersistableSashForm;
 
 public class GCConfigurationPage extends AbstractDataPage {
 	public static class GCConfigurationPageFactory implements IDataPageFactory {
@@ -84,15 +104,69 @@ public class GCConfigurationPage extends AbstractDataPage {
 		}
 
 	}
+	
+	
+	
+	private static final String JVM_FLAGS = "jvmFlags"; //$NON-NLS-1$
+	private static final Set<String> FLAGS;
+
+	static {
+		Set<String> types = new HashSet<>();
+		types.add(JdkTypeIDs.LONG_FLAG);
+		types.add(JdkTypeIDs.ULONG_FLAG);
+		types.add(JdkTypeIDs.DOUBLE_FLAG);
+		types.add(JdkTypeIDs.BOOLEAN_FLAG);
+		types.add(JdkTypeIDs.STRING_FLAG);
+		types.add(JdkTypeIDs.INT_FLAG);
+		types.add(JdkTypeIDs.UINT_FLAG);
+		FLAGS = Collections.unmodifiableSet(types);
+	}
+	private static final IItemFilter FLAGS_FILTER = ItemFilters.type(FLAGS);
+	private static final IAccessorFactory<?> FLAG_VALUE_FIELD = new IAccessorFactory<Object>() {
+
+		@Override
+		public <T> IMemberAccessor<?, T> getAccessor(IType<T> type) {
+			switch (type.getIdentifier()) {
+			case JdkTypeIDs.LONG_FLAG:
+			case JdkTypeIDs.ULONG_FLAG:
+			case JdkTypeIDs.DOUBLE_FLAG:
+			case JdkTypeIDs.INT_FLAG:
+			case JdkTypeIDs.UINT_FLAG:
+				return JdkAttributes.FLAG_VALUE_NUMBER.getAccessor(type);
+			case JdkTypeIDs.BOOLEAN_FLAG:
+				return JdkAttributes.FLAG_VALUE_BOOLEAN.getAccessor(type);
+			case JdkTypeIDs.STRING_FLAG:
+				return JdkAttributes.FLAG_VALUE_TEXT.getAccessor(type);
+			default:
+				// FIXME: Return fallback function instead?
+				return null;
+			}
+		}
+
+	};
+    private static final String FLAG_VALUE_COL_ID = "value"; //$NON-NLS-1$
+	private static final CompositeKeyHistogramBuilder FLAG_HISTOGRAM = new CompositeKeyHistogramBuilder();
+	static {
+		FLAG_HISTOGRAM.addKeyColumn(JdkAttributes.FLAG_NAME);
+		FLAG_HISTOGRAM.addKeyColumn(JdkAttributes.FLAG_ORIGIN);
+		FLAG_HISTOGRAM.addKeyColumn(FLAG_VALUE_COL_ID, Messages.JVMInformationPage_COLUMN_VALUE, FLAG_VALUE_FIELD);
+	}
+	private SashForm flagSash;
+	private ItemHistogram allFlagsTable;
+	private IItemFilter flagsFilter;
+	private FilterComponent allFlagsFilter;
+
 
 	@Override
 	public IPageUI display(Composite parent, FormToolkit toolkit, IPageContainer pageContainer, IState state) {
 		Form form = DataPageToolkit.createForm(parent, toolkit, getName(), getIcon());
-		SashForm container = new SashForm(form.getBody(), SWT.HORIZONTAL);
-		container.setSashWidth(5);
-		container.addTraverseListener(new SimpleTraverseListener());
+		SashForm container = new SashForm(form.getBody(), SWT.VERTICAL);
+		
+		SashForm gcConfigSash = new SashForm(container, SWT.HORIZONTAL);
+		gcConfigSash.setSashWidth(5);
+		gcConfigSash.addTraverseListener(new SimpleTraverseListener());
 
-		Section gcConfigSection = CompositeToolkit.createSection(container, toolkit,
+		Section gcConfigSection = CompositeToolkit.createSection(gcConfigSash, toolkit,
 				Messages.GCConfigurationPage_SECTION_GC_CONFIG);
 		ItemAggregateViewer gcConfig = new ItemAggregateViewer(gcConfigSection, toolkit);
 		gcConfig.addAggregate(JdkAggregators.YOUNG_COLLECTOR);
@@ -105,7 +179,7 @@ public class GCConfigurationPage extends AbstractDataPage {
 		gcConfig.addAggregate(JdkAggregators.GC_TIME_RATIO_MIN);
 		gcConfigSection.setClient(gcConfig.getControl());
 
-		Section heapConfigSection = CompositeToolkit.createSection(container, toolkit,
+		Section heapConfigSection = CompositeToolkit.createSection(gcConfigSash, toolkit,
 				Messages.GCConfigurationPage_SECTION_HEAP_CONFIG);
 		ItemAggregateViewer heapConfig = new ItemAggregateViewer(heapConfigSection, toolkit);
 		heapConfig.addAggregate(JdkAggregators.HEAP_CONF_INITIAL_SIZE_MIN);
@@ -117,7 +191,7 @@ public class GCConfigurationPage extends AbstractDataPage {
 		heapConfig.addAggregate(JdkAggregators.HEAP_OBJECT_ALIGNMENT_MIN);
 		heapConfigSection.setClient(heapConfig.getControl());
 
-		Section ycConfigSection = CompositeToolkit.createSection(container, toolkit,
+		Section ycConfigSection = CompositeToolkit.createSection(gcConfigSash, toolkit,
 				Messages.GCConfigurationPage_SECTION_YOUNG_CONFIG);
 		ItemAggregateViewer ycConfig = new ItemAggregateViewer(ycConfigSection, toolkit);
 		ycConfig.addAggregate(JdkAggregators.YOUNG_GENERATION_MIN_SIZE);
@@ -133,11 +207,47 @@ public class GCConfigurationPage extends AbstractDataPage {
 		gcConfig.setValues(getDataSource().getItems());
 		heapConfig.setValues(getDataSource().getItems());
 		ycConfig.setValues(getDataSource().getItems());
+		
+		
+		flagSash = new SashForm(container, SWT.VERTICAL);
+		toolkit.adapt(flagSash);
 
+		// put below (sashform)
+		Section allFlagsSection = CompositeToolkit.createSection(flagSash, toolkit,
+				Messages.JVMInformationPage_SECTION_JVM_FLAGS);
+		allFlagsTable = FLAG_HISTOGRAM.buildWithoutBorder(allFlagsSection,
+				null);
+//				new TableSettings(state.getChild(JVM_FLAGS)));
+		allFlagsFilter = FilterComponent.createFilterComponent(allFlagsTable, flagsFilter,
+				getDataSource().getItems().apply(FLAGS_FILTER), pageContainer.getSelectionStore()::getSelections,
+				this::onFlagsFilterChange);
+		MCContextMenuManager flagsMm = MCContextMenuManager
+				.create(allFlagsTable.getManager().getViewer().getControl());
+		ColumnMenusFactory.addDefaultMenus(allFlagsTable.getManager(), flagsMm);
+		flagsMm.add(allFlagsFilter.getShowFilterAction());
+		flagsMm.add(allFlagsFilter.getShowSearchAction());
+		allFlagsSection.setClient(allFlagsFilter.getComponent());
+
+		ColumnViewer flagViewer = allFlagsTable.getManager().getViewer();
+		flagViewer.addSelectionChangedListener(
+				e -> pageContainer.showSelection(allFlagsTable.getSelection().getItems()));
+		
+		
+		
+		allFlagsTable.show(getDataSource().getItems().apply(FLAGS_FILTER));
+		onFlagsFilterChange(flagsFilter);
+		// allFlagsTable.getManager().setSelectionState(flagsSelection);
+		
 		addResultActions(form);
 
 		return null;
 	}
+	
+	private void onFlagsFilterChange(IItemFilter filter) {
+		allFlagsFilter.filterChangeHelper(filter, allFlagsTable, getDataSource().getItems().apply(FLAGS_FILTER));
+		flagsFilter = filter;
+	}
+
 
 	public GCConfigurationPage(IPageDefinition dpd, StreamModel items, IPageContainer editor) {
 		super(dpd, items, editor);
@@ -145,6 +255,6 @@ public class GCConfigurationPage extends AbstractDataPage {
 
 	@Override
 	public IItemFilter getDefaultSelectionFilter() {
-		return JdkFilters.GC_CONFIG;
+		return ItemFilters.or(FLAGS_FILTER, JdkFilters.GC_CONFIG);
 	}
 }
