9dced59b33fb48a973f9560f261e4bfacfe88711
Oskar Jung <jung.oskar@gmail.com>
2021-05-31 20:16:33 +0200

    7262: Add rule for when the JVM version is < OpenJDK 8u302
    Since the test flight recording was created with 8u302 EA, a method
    `JavaVersion.isGreaterOrEqualThanDisregardEa` had to be added to compare
    java versions disregarding early access status.

---
+++ core/org.openjdk.jmc.flightrecorder.rules.jdk/src/main/java/org/openjdk/jmc/flightrecorder/rules/jdk/general/JfrPeriodicEventsFixRule.java
@@ -0,0 +1,157 @@
+/*
+ * Copyright (c) 2021 Oracle and/or its affiliates. All rights reserved.
+ *
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
+ *
+ * The contents of this file are subject to the terms of either the Universal Permissive License
+ * v 1.0 as shown at http://oss.oracle.com/licenses/upl
+ *
+ * or the following license:
+ *
+ * Redistribution and use in source and binary forms, with or without modification, are permitted
+ * provided that the following conditions are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright notice, this list of conditions
+ * and the following disclaimer.
+ *
+ * 2. Redistributions in binary form must reproduce the above copyright notice, this list of
+ * conditions and the following disclaimer in the documentation and/or other materials provided with
+ * the distribution.
+ *
+ * 3. Neither the name of the copyright holder nor the names of its contributors may be used to
+ * endorse or promote products derived from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR
+ * IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND
+ * FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR
+ * CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
+ * WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY
+ * WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+package org.openjdk.jmc.flightrecorder.rules.jdk.general;
+
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.Map;
+import java.util.concurrent.Callable;
+import java.util.concurrent.FutureTask;
+import java.util.concurrent.RunnableFuture;
+import java.util.regex.Pattern;
+
+import org.openjdk.jmc.common.item.Aggregators;
+import org.openjdk.jmc.common.item.IItemCollection;
+import org.openjdk.jmc.common.unit.UnitLookup;
+import org.openjdk.jmc.common.util.IPreferenceValueProvider;
+import org.openjdk.jmc.common.util.TypedPreference;
+import org.openjdk.jmc.common.version.JavaVersion;
+import org.openjdk.jmc.flightrecorder.jdk.JdkAttributes;
+import org.openjdk.jmc.flightrecorder.jdk.JdkTypeIDs;
+import org.openjdk.jmc.flightrecorder.rules.IResult;
+import org.openjdk.jmc.flightrecorder.rules.IResultValueProvider;
+import org.openjdk.jmc.flightrecorder.rules.IRule;
+import org.openjdk.jmc.flightrecorder.rules.ResultBuilder;
+import org.openjdk.jmc.flightrecorder.rules.Severity;
+import org.openjdk.jmc.flightrecorder.rules.TypedResult;
+import org.openjdk.jmc.flightrecorder.rules.jdk.messages.internal.Messages;
+import org.openjdk.jmc.flightrecorder.rules.util.JfrRuleTopics;
+import org.openjdk.jmc.flightrecorder.rules.util.RulesToolkit;
+import org.openjdk.jmc.flightrecorder.rules.util.RulesToolkit.EventAvailability;
+import org.openjdk.jmc.flightrecorder.rules.util.RulesToolkit.RequiredEventsBuilder;
+
+public class JfrPeriodicEventsFixRule implements IRule {
+
+	private static final String RESULT_ID = "JfrPeriodicEventsFix"; //$NON-NLS-1$
+
+	private static final Map<String, EventAvailability> REQUIRED_EVENTS = RequiredEventsBuilder.create()
+			.addEventType(JdkTypeIDs.VM_INFO, EventAvailability.AVAILABLE).build();
+
+	public static final TypedResult<String> JDK_VERSION = new TypedResult<>("jdkVersion", "JDK Version", //$NON-NLS-1$
+			"The version of the JDK that produced the recording.", UnitLookup.PLAIN_TEXT, String.class);
+
+	private static final Collection<TypedResult<?>> RESULT_ATTRIBUTES = Arrays.<TypedResult<?>> asList(JDK_VERSION);
+
+	private static final JavaVersion JDK_8_U_302 = new JavaVersion(8, 0, 302);
+
+	private static final Pattern OPENJDK_VM_NAME_REGEX = Pattern.compile("^OpenJDK.*$");
+
+	private IResult getResult(
+		IItemCollection items, IPreferenceValueProvider valueProvider, IResultValueProvider resultProvider) {
+		String vmName = items.getAggregate(Aggregators.distinctAsString(JdkTypeIDs.VM_INFO, JdkAttributes.JVM_NAME));
+		boolean isOpenJdk = OPENJDK_VM_NAME_REGEX.matcher(vmName).matches();
+		if (!isOpenJdk) {
+			return ResultBuilder.createFor(this, valueProvider).setSeverity(Severity.OK)
+					.setSummary(Messages.getString(Messages.JfrPeriodicEventsFixRule_TEXT_OK)).build();
+		}
+
+		String jvmVersion = items
+				.getAggregate(Aggregators.distinctAsString(JdkTypeIDs.VM_INFO, JdkAttributes.JVM_VERSION));
+		if (jvmVersion == null) {
+			return RulesToolkit.getNotApplicableResult(this, valueProvider,
+					Messages.getString(Messages.JfrPeriodicEventsFixRule_NO_JVM_VERSION_EVENTS_TEXT));
+		}
+
+		JavaVersion usedVersion = RulesToolkit.getJavaVersion(jvmVersion);
+
+		if (usedVersion == null) {
+			return RulesToolkit.getNotApplicableResult(this, valueProvider,
+					Messages.getString(Messages.General_TEXT_COULD_NOT_DETERMINE_JAVA_VERSION));
+		}
+
+		if (!usedVersion.isGreaterOrEqualThanDisregardEa(JDK_8_U_302)) {
+			return ResultBuilder.createFor(this, valueProvider).setSeverity(Severity.WARNING)
+					.addResult(JDK_VERSION, jvmVersion)
+					.setSummary(Messages.getString(Messages.JfrPeriodicEventsFixRule_TEXT_WARN_SHORT))
+					.setExplanation(Messages.getString(Messages.JfrPeriodicEventsFixRule_TEXT_WARN_LONG))
+					.build();
+		}
+
+		return ResultBuilder.createFor(this, valueProvider).setSeverity(Severity.OK)
+				.setSummary(Messages.getString(Messages.JfrPeriodicEventsFixRule_TEXT_OK)).build();
+	}
+
+	@Override
+	public RunnableFuture<IResult> createEvaluation(
+		final IItemCollection items, final IPreferenceValueProvider valueProvider,
+		final IResultValueProvider resultProvider) {
+		FutureTask<IResult> evaluationTask = new FutureTask<>(new Callable<IResult>() {
+			@Override
+			public IResult call() throws Exception {
+				return getResult(items, valueProvider, resultProvider);
+			}
+		});
+		return evaluationTask;
+	}
+
+	@Override
+	public Collection<TypedPreference<?>> getConfigurationAttributes() {
+		return Collections.emptyList();
+	}
+
+	@Override
+	public String getId() {
+		return RESULT_ID;
+	}
+
+	@Override
+	public String getName() {
+		return Messages.getString(Messages.JfrPeriodicEventsFixRule_RULE_NAME);
+	}
+
+	@Override
+	public String getTopic() {
+		return JfrRuleTopics.JVM_INFORMATION;
+	}
+
+	@Override
+	public Map<String, EventAvailability> getRequiredEvents() {
+		return REQUIRED_EVENTS;
+	}
+
+	@Override
+	public Collection<TypedResult<?>> getResults() {
+		return RESULT_ATTRIBUTES;
+	}
+}
