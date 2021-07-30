ad9b602d9219c726239f848672ee27da96611254
Guru Hb <ghb@openjdk.org>
2021-07-30 13:33:25 +0000

    7337: JMC fails to parse JFR with events from WebLogic Server
    Reviewed-by: hirt

--- core/org.openjdk.jmc.flightrecorder/src/main/java/org/openjdk/jmc/flightrecorder/parser/synthetic/SyntheticAttributeExtension.java
+++ core/org.openjdk.jmc.flightrecorder/src/main/java/org/openjdk/jmc/flightrecorder/parser/synthetic/SyntheticAttributeExtension.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2018, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2018, 2021, Oracle and/or its affiliates. All rights reserved.
  *
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
@@ -203,11 +203,13 @@ public class SyntheticAttributeExtension implements IParserExtension {
 		@Override
 		public void addEvent(Object[] values) {
 			IMCPackage thePackage = (IMCPackage) values[packageFieldIndex];
-			IMCModule exportingModule = thePackage.getModule();
-			Object[] newValues = new Object[values.length + 1];
-			System.arraycopy(values, 0, newValues, 0, values.length);
-			newValues[values.length] = exportingModule;
-			subSink.addEvent(newValues);
+			if (thePackage != null) {
+				IMCModule exportingModule = thePackage.getModule();
+				Object[] newValues = new Object[values.length + 1];
+				System.arraycopy(values, 0, newValues, 0, values.length);
+				newValues[values.length] = exportingModule;
+				subSink.addEvent(newValues);
+			}
 		}
 	}
 }
