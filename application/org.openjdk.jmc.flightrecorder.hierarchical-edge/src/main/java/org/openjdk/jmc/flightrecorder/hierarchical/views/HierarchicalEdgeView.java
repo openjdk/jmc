e1d2754ccd253f59c0051cbc3472b371c3e53bc9
Alex Ciminian <alexandru.ciminian@datadoghq.com>
2022-06-27 12:47:07 +0100

    Fix package name in HierarchicalEdgeView

--- application/org.openjdk.jmc.flightrecorder.hierarchical-edge/src/main/java/org/openjdk/jmc/flightrecorder/hierarchical/views/HierarchicalEdgeView.java
+++ application/org.openjdk.jmc.flightrecorder.hierarchical-edge/src/main/java/org/openjdk/jmc/flightrecorder/hierarchical/views/HierarchicalEdgeView.java
@@ -31,7 +31,7 @@
  * WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY
  * WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
-package org.openjdk.jmc.flightrecorder.heatmap.views;
+package org.openjdk.jmc.flightrecorder.hierarchical.views;
 
 import java.io.BufferedInputStream;
 import java.io.ByteArrayOutputStream;
