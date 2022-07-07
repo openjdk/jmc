0889d60dea0cd6c2f6bc29ca8fb84ccf4758d1df
Christoph Langer <clanger@openjdk.org>
2022-07-07 07:56:29 +0000

    7834: Improve GitHub Actions validation workflow
    Reviewed-by: hirt

--- scripts/runagenttests.sh
+++ scripts/runagenttests.sh
@@ -1,8 +1,12 @@
 #!/bin/sh -l
-
 set -e
-echo "======== Running agent tests ================"
-cd agent
-mvn verify
-echo "======== Finished ==========================="
 
+echo "======== Building and testing agent ========="
+cd agent
+# The integration tests currently fail on windows - change to "mvn verify" once fixed.
+if [[ $(uname) == MINGW* ]] || [[ $(uname) == CYGWIN* ]]; then
+  sh -c "mvn ${MAVENPARAMS} test"
+else
+  sh -c "mvn ${MAVENPARAMS} verify"
+fi
+echo "======== Finished ==========================="
