056139f05ae8406c7203a68543c9e9041240427f
Ralf Schmelter <rschmelter@openjdk.org>
2022-07-14 12:40:57 +0000

    7842: JMC agent: policy file is missing entry for test on Windows
    Reviewed-by: clanger

--- scripts/runagenttests.bat
+++ scripts/runagenttests.bat
@@ -2,6 +2,5 @@
 
 echo "======== Building and testing agent ========="
 cd agent
-rem The integration tests currently fail on windows - change to "mvn verify" once fixed.
-call mvn %MAVENPARAMS% test || EXIT /B 4
+call mvn %MAVENPARAMS% verify || EXIT /B 4
 echo "======== Finished ==========================="
--- scripts/runagenttests.sh
+++ scripts/runagenttests.sh
@@ -3,10 +3,5 @@ set -e
 
 echo "======== Building and testing agent ========="
 cd agent
-# The integration tests currently fail on windows - change to "mvn verify" once fixed.
-if [[ $(uname) == MINGW* ]] || [[ $(uname) == CYGWIN* ]]; then
-  sh -c "mvn ${MAVENPARAMS} test"
-else
-  sh -c "mvn ${MAVENPARAMS} verify"
-fi
+sh -c "mvn ${MAVENPARAMS} verify"
 echo "======== Finished ==========================="
