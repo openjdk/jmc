2dff3ea11335f0395b2edd6894cfba3243d2440d
Henrik Dafgård <hdafgard@gmail.com>
2022-09-28 23:52:05 +0200

    Fix comparison in UseCompressedOopsRule

--- core/org.openjdk.jmc.flightrecorder.rules.jdk/src/main/java/org/openjdk/jmc/flightrecorder/rules/jdk/memory/CompressedOopsRule.java
+++ core/org.openjdk.jmc.flightrecorder.rules.jdk/src/main/java/org/openjdk/jmc/flightrecorder/rules/jdk/memory/CompressedOopsRule.java
@@ -77,7 +77,7 @@ public class CompressedOopsRule implements IRule {
 		String vmName = items.getAggregate(JdkAggregators.JVM_NAME);
 		if (compressedOops != null && mx != null && vmName != null) {
 			if (vmName.toUpperCase().contains("64-BIT")) { //$NON-NLS-1$
-				if (mx.compareTo(UnitLookup.NUMBER.getUnit(BinaryPrefix.GIBI).quantity(32)) > 0) {
+				if (mx.compareTo(UnitLookup.NUMBER.getUnit(BinaryPrefix.GIBI).quantity(32)) < 0) {
 					if (!compressedOops) {
 						return ResultBuilder.createFor(this, valueProvider).setSeverity(Severity.INFO)
 								.setSummary(Messages.getString(Messages.CompressedOopsRuleFactory_TEXT_INFO))
