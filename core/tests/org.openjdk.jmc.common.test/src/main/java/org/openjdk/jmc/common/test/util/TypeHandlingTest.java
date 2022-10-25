f2e48799131ca5744020f9a444e9bd2cd8441f4b
Christoph Langer <christoph.langer@sap.com>
2022-10-25 20:29:24 +0200

    JMC-7946
    Fix project core/tests/org.openjdk.jmc.common.test to make it run tests again

--- core/tests/org.openjdk.jmc.common.test/src/main/java/org/openjdk/jmc/common/test/util/TypeHandlingTest.java
+++ core/tests/org.openjdk.jmc.common.test/src/main/java/org/openjdk/jmc/common/test/util/TypeHandlingTest.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2021, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2021, 2022, Oracle and/or its affiliates. All rights reserved.
  *
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
@@ -30,11 +30,12 @@
  * WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY
  * WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
-package org.openjdk.jmc.common.util;
-
-import static org.junit.Assert.assertEquals;
+package org.openjdk.jmc.common.test.util;
 
 import org.junit.Test;
+import org.openjdk.jmc.common.util.TypeHandling;
+
+import static org.junit.Assert.assertEquals;
 
 @SuppressWarnings("nls")
 public class TypeHandlingTest {
@@ -49,5 +50,4 @@ public class TypeHandlingTest {
 		assertEquals("N/A (Float.NaN)", TypeHandling.getNumericString(Float.NaN));
 		assertEquals("N/A (Float.NEGATIVE_INFINITY)", TypeHandling.getNumericString(Float.NEGATIVE_INFINITY));
 	}
-
 }
--- core/tests/org.openjdk.jmc.common.test/src/main/java/org/openjdk/jmc/common/test/version/JavaVersionTest.java
+++ core/tests/org.openjdk.jmc.common.test/src/main/java/org/openjdk/jmc/common/test/version/JavaVersionTest.java
@@ -1,6 +1,6 @@
 /*
- * Copyright (c) 2018, Oracle and/or its affiliates. All rights reserved.
- * 
+ * Copyright (c) 2018, 2022, Oracle and/or its affiliates. All rights reserved.
+ *
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * The contents of this file are subject to the terms of either the Universal Permissive License
@@ -10,17 +10,17 @@
  *
  * Redistribution and use in source and binary forms, with or without modification, are permitted
  * provided that the following conditions are met:
- * 
+ *
  * 1. Redistributions of source code must retain the above copyright notice, this list of conditions
  * and the following disclaimer.
- * 
+ *
  * 2. Redistributions in binary form must reproduce the above copyright notice, this list of
  * conditions and the following disclaimer in the documentation and/or other materials provided with
  * the distribution.
- * 
+ *
  * 3. Neither the name of the copyright holder nor the names of its contributors may be used to
  * endorse or promote products derived from this software without specific prior written permission.
- * 
+ *
  * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR
  * IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND
  * FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR
@@ -30,13 +30,14 @@
  * WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY
  * WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
-package org.openjdk.jmc.common.version;
+package org.openjdk.jmc.common.test.version;
+
+import org.junit.Test;
+import org.openjdk.jmc.common.version.JavaVersion;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
-
-import org.junit.Test;
 
 public class JavaVersionTest {
 
