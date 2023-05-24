5ee9b3fb5c59bd78654c8246a7d7218d46b69597
Christoph Langer <clanger@openjdk.org>
2023-05-24 18:09:38 +0000

    8071: Cleanup after latest refactorings
    Reviewed-by: bdutheil, aptmac, vpurnam

--- core/org.openjdk.jmc.common/src/main/java/org/openjdk/jmc/common/security/ISecurityManager.java
+++ core/org.openjdk.jmc.common/src/main/java/org/openjdk/jmc/common/security/ISecurityManager.java
@@ -1,6 +1,6 @@
 /*
  * Copyright (c) 2018, 2023, Oracle and/or its affiliates. All rights reserved.
- * 
+ *
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * The contents of this file are subject to the terms of either the Universal Permissive License
@@ -10,17 +10,17 @@
  *
  * Redistribution and use in source and binary forms, with or without modification, are permitted
  * provided that the following conditions are met:
- * 
+ *
  * 1. Redistributions of source code must retain the above copyright notice, this list of conditions
  * and the following disclaimer.
- * 
+ *
  * 2. Redistributions in binary form must reproduce the above copyright notice, this list of
  * conditions and the following disclaimer in the documentation and/or other materials provided with
  * the distribution.
- * 
+ *
  * 3. Neither the name of the copyright holder nor the names of its contributors may be used to
  * endorse or promote products derived from this software without specific prior written permission.
- * 
+ *
  * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR
  * IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND
  * FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR
@@ -190,25 +190,18 @@ public interface ISecurityManager {
 	 *
 	 * @param encryptionCipher
 	 *            the encryption cipher to use.
-	 * @throws CipherNotAvailableException
-	 *             if the provided cipher is not available.
-	 * @throws ActionNotGrantedException
-	 *             if the master password was not given.
 	 * @throws SecurityException
-	 *             for other security problems.
+	 *             for security problems, e.g. provided cipher is not available.
 	 */
 	void setEncryptionCipher(String encryptionCipher) throws SecurityException;
 
 	/**
 	 * Triggers a change of the master password.
 	 *
-	 * @throws CipherNotAvailableException
-	 *             if no cipher is available.
 	 * @throws ActionNotGrantedException
 	 *             if the master password was not given.
 	 * @throws SecurityException
-	 *             for other security problems.
+	 *             for other security problems, e.g. provided cipher is not available.
 	 */
 	void changeMasterPassword() throws SecurityException;
-
 }
