0d0df70a277c2373894758d6695a97b44a71e6f8
Marcus Hirt <hirt@openjdk.org>
2023-11-08 18:54:06 +0000

    8129: Add tool to simplify platform upgrades
    Reviewed-by: aptmac, vpurnam

---
+++ releng/tools/org.openjdk.jmc.util.listversions/src/org/openjdk/jmc/util/listversions/ListVersions.java
@@ -0,0 +1,119 @@
+/*
+ * Copyright (c) 2023, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2023, Datadog, Inc. All rights reserved.
+ *
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
+ *
+ * The contents of this file are subject to the terms of either the Universal Permissive License
+ * v 1.0 as shown at http://oss.oracle.com/licenses/upl
+ *
+ * or the following license:
+ *
+ * Redistribution and use in source and binary forms, with or without modification, are permitted
+ * provided that the following conditions are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright notice, this list of conditions
+ * and the following disclaimer.
+ *
+ * 2. Redistributions in binary form must reproduce the above copyright notice, this list of
+ * conditions and the following disclaimer in the documentation and/or other materials provided with
+ * the distribution.
+ *
+ * 3. Neither the name of the copyright holder nor the names of its contributors may be used to
+ * endorse or promote products derived from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR
+ * IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND
+ * FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR
+ * CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
+ * WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY
+ * WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+package org.openjdk.jmc.util.listversions;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URL;
+import java.util.zip.ZipInputStream;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+
+public class ListVersions {
+
+	public static void main(String[] args) throws IOException {
+		if (args.length != 1) {
+			System.out.println("You need to specify the Eclipse version, e.g. 2023-09!");
+			System.exit(2);
+		}
+		String eclipseVersion = args[0];
+
+		String updateSite = String.format("https://download.eclipse.org/releases/%s/", eclipseVersion);
+		String compositeArtifactsUrl = updateSite + "compositeArtifacts.jar";
+
+		try (InputStream compositeStream = new URL(compositeArtifactsUrl).openStream();
+				ZipInputStream compositeZipStream = new ZipInputStream(compositeStream)) {
+			compositeZipStream.getNextEntry();
+
+			DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
+			DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
+			Document compositeDoc = dBuilder.parse(compositeZipStream);
+
+			NodeList childrenList = compositeDoc.getElementsByTagName("child");
+			String subDirectory = null;
+
+			for (int i = 0; i < childrenList.getLength(); i++) {
+				Node childNode = childrenList.item(i);
+				if (childNode.getNodeType() == Node.ELEMENT_NODE) {
+					Element childElement = (Element) childNode;
+					subDirectory = childElement.getAttribute("location");
+					if (Character.isDigit(subDirectory.charAt(0))) {
+						break;
+					}
+				}
+			}
+
+			if (subDirectory == null) {
+				System.out.println("Failed to find subdirectory.");
+				return;
+			}
+
+			String contentJarUrl = updateSite + subDirectory + "/content.jar";
+
+			try (InputStream contentStream = new URL(contentJarUrl).openStream();
+					ZipInputStream contentZipStream = new ZipInputStream(contentStream)) {
+				contentZipStream.getNextEntry();
+
+				Document contentDoc = dBuilder.parse(contentZipStream);
+
+				NodeList unitList = contentDoc.getElementsByTagName("unit");
+				for (int i = 0; i < unitList.getLength(); i++) {
+					Node unitNode = unitList.item(i);
+					if (unitNode.getNodeType() == Node.ELEMENT_NODE) {
+						Element unitElement = (Element) unitNode;
+						String id = unitElement.getAttribute("id");
+						String version = unitElement.getAttribute("version");
+						switch (id) {
+						case "org.eclipse.equinox.executable.feature.group":
+						case "org.eclipse.pde.feature.group":
+						case "org.eclipse.platform.sdk":
+							System.out.println("Found unit: " + id + ", Version: " + version);
+							break;
+						default:
+							// Ignoring other units
+						}
+					}
+				}
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+}
