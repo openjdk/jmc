449a7ec13bd5150c2e252637aaf4826c58d1308c
Martin Skarsaune <martin@skarsaune.net>
2024-03-07 08:18:47 +0100

    JMC-7455: Pick up  protocol extension and use it

--- application/org.openjdk.jmc.rjmx/src/main/java/org/openjdk/jmc/rjmx/internal/ProtocolInitializer.java
+++ application/org.openjdk.jmc.rjmx/src/main/java/org/openjdk/jmc/rjmx/internal/ProtocolInitializer.java
@@ -34,17 +34,11 @@
 package org.openjdk.jmc.rjmx.internal;
 
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.List;
 import java.util.Map;
 import java.util.logging.Level;
-import java.util.logging.Logger;
 
 import javax.management.remote.JMXConnector;
-import javax.management.remote.JMXConnectorFactory;
 import javax.management.remote.JMXConnectorProvider;
-import javax.management.remote.JMXConnectorServerFactory;
 import javax.management.remote.JMXServiceURL;
 
 import org.eclipse.core.runtime.CoreException;
@@ -52,10 +46,6 @@ import org.eclipse.core.runtime.IConfigurationElement;
 import org.eclipse.core.runtime.IExtensionRegistry;
 import org.eclipse.core.runtime.Platform;
 import org.openjdk.jmc.rjmx.RJMXPlugin;
-import org.osgi.framework.BundleContext;
-import org.osgi.framework.FrameworkUtil;
-import org.osgi.framework.InvalidSyntaxException;
-import org.osgi.framework.ServiceReference;
 
 public class ProtocolInitializer {
 	private final static String EXTENSION_POINT = "org.openjdk.jmc.rjmx.jmxProtocols";//$NON-NLS-1$
@@ -69,58 +59,32 @@ public class ProtocolInitializer {
 
 	public JMXConnector newJMXConnector(JMXServiceURL serviceURL, Map<String, ?> environment) throws IOException {
 		@SuppressWarnings("unchecked")
-		JMXConnectorProvider realProvider = extendEnv(serviceURL, (Map<String, Object>) environment);
+		JMXConnectorProvider realProvider = findProtocolExtension(serviceURL);
 		if (realProvider == null) {
 			return null;
 		}
 		return realProvider.newJMXConnector(serviceURL, environment);
 	}
 
-	public JMXConnectorProvider extendEnv(JMXServiceURL serviceURL, Map<String, Object> environment) {
-		String filter = '(' + EXTENSION_ATTRIBUTE_PROTOCOL + '=' + serviceURL.getProtocol() + ')';
+	public JMXConnectorProvider findProtocolExtension(JMXServiceURL serviceURL) {
 		try {
-			final BundleContext context = FrameworkUtil.getBundle(getClass()).getBundleContext();
-			Collection<ServiceReference<JMXConnectorProvider>> refs;
-			refs = context.getServiceReferences(JMXConnectorProvider.class, filter);
-
-			for (ServiceReference<JMXConnectorProvider> ref : refs) {
-				JMXConnectorProvider provider = context.getService(ref);
-				if (provider != null) {
-					for (String key : ref.getPropertyKeys()) {
-						Object value = ref.getProperty(key);
-
-						String property = getIfPrefixed("environment:", key); //$NON-NLS-1$
-						if (property != null) {
-							environment.put(property, value);
+			IExtensionRegistry registry = Platform.getExtensionRegistry();
+			for (IConfigurationElement config : registry.getConfigurationElementsFor(EXTENSION_POINT)) {
+				if (config.getName().equals(EXTENSION_ELEMENT_CLIENT)) {
+					final String protocol = config.getAttribute(EXTENSION_ATTRIBUTE_PROTOCOL);
+					if (serviceURL.getProtocol().equals(protocol)) {
+						JMXConnectorProvider provider = (JMXConnectorProvider) config
+								.createExecutableExtension(EXTENSION_ATTRIBUTE_CLASS);
+						for (IConfigurationElement prop : config.getChildren(TAG_SYSPROPERTY)) {
+							ensureSystemProperty(prop.getAttribute(ATTRIBUTE_KEY), prop.getAttribute(ATTRIBUTE_INCLUDE),
+									prop.getAttribute(ATTRIBUTE_SEPARATOR));
 						}
-
-						property = getIfPrefixed("system:", key); //$NON-NLS-1$
-						if (property != null) {
-							ensureSystemProperty(property, value.toString(), "|"); //$NON-NLS-1$
-						}
+						return provider;
 					}
-
-					ClassLoader loader = provider.getClass().getClassLoader();
-					environment.put(JMXConnectorFactory.DEFAULT_CLASS_LOADER, loader);
-					// Used by MX4J.
-					environment.put(JMXConnectorServerFactory.PROTOCOL_PROVIDER_CLASS_LOADER, loader);
-
-					// FIXME: Add WeakRef/ReferenceQueue or listener to "unget" service?
-					// return provider.newJMXConnector(serviceURL, environment);
-					return provider;
 				}
 			}
-		} catch (InvalidSyntaxException e) {
-			// Assume this is a bad protocol name. At least we don't support it.
-			Logger.getLogger("org.openjdk.jmc.rjmx.ext").log(Level.WARNING, "Bad protocol syntax", e); //$NON-NLS-1$ //$NON-NLS-2$
-		}
-
-		return null;
-	}
-
-	private static String getIfPrefixed(String prefix, String candidate) {
-		if (candidate.startsWith(prefix)) {
-			return candidate.substring(prefix.length());
+		} catch (CoreException e) {
+			RJMXPlugin.getDefault().getLogger().log(Level.SEVERE, "Could not instantiate JMX protocol provider!", e);
 		}
 		return null;
 	}
@@ -135,27 +99,4 @@ public class ProtocolInitializer {
 			System.setProperty(key, org + separator + include);
 		}
 	}
-
-	public static List<JMXConnectorProvider> initializeFromExtensions() {
-		List<JMXConnectorProvider> providers = new ArrayList<>();
-		IExtensionRegistry registry = Platform.getExtensionRegistry();
-		for (IConfigurationElement config : registry.getConfigurationElementsFor(EXTENSION_POINT)) {
-			if (config.getName().equals(EXTENSION_ELEMENT_CLIENT)) {
-				try {
-					JMXConnectorProvider provider = (JMXConnectorProvider) config
-							.createExecutableExtension(EXTENSION_ATTRIBUTE_CLASS);
-					providers.add(provider);
-					for (IConfigurationElement prop : config.getChildren(TAG_SYSPROPERTY)) {
-						ensureSystemProperty(prop.getAttribute(ATTRIBUTE_KEY), prop.getAttribute(ATTRIBUTE_INCLUDE),
-								prop.getAttribute(ATTRIBUTE_SEPARATOR));
-					}
-				} catch (CoreException e) {
-					RJMXPlugin.getDefault().getLogger().log(Level.SEVERE,
-							"Could not instantiate JMX protocol provider!", e);
-				}
-			}
-		}
-		return providers;
-	}
-
 }
