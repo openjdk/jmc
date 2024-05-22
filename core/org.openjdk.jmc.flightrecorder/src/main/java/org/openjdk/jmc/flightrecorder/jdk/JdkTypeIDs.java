5a724c25b28d8ac9cb52dff4bf9d06e601788e23
schaturvedi <suchita.chaturvedi@oracle.com>
2024-05-22 23:10:06 +0530

    Revert "Corrected the content type from Long to String for certificate Id."
    This reverts commit 4da9005c4974596eb04ac9a5833ba81a08980bcb.

--- core/org.openjdk.jmc.flightrecorder/src/main/java/org/openjdk/jmc/flightrecorder/jdk/JdkTypeIDs.java
+++ core/org.openjdk.jmc.flightrecorder/src/main/java/org/openjdk/jmc/flightrecorder/jdk/JdkTypeIDs.java
@@ -72,7 +72,6 @@ public final class JdkTypeIDs {
 	public static final String CLASS_LOAD_STATISTICS = PREFIX + "ClassLoadingStatistics";
 	public static final String CLASS_LOADER_STATISTICS = PREFIX + "ClassLoaderStatistics";
 	public static final String COMPILATION = PREFIX + "Compilation";
-	public static final String X509_CERTIFICATE = PREFIX + "X509Certificate";
 
 	public static final String FILE_WRITE = PREFIX + "FileWrite";
 	public static final String FILE_READ = PREFIX + "FileRead";
