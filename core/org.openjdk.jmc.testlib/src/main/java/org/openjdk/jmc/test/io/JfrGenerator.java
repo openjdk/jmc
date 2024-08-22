872ddf3aa0eef816223596b8f7e82cbff0375f03
Carter Kozak <ckozak@ckozak.net>
2024-08-22 17:01:18 -0400

    spotless

--- core/org.openjdk.jmc.testlib/src/main/java/org/openjdk/jmc/test/io/JfrGenerator.java
+++ core/org.openjdk.jmc.testlib/src/main/java/org/openjdk/jmc/test/io/JfrGenerator.java
@@ -42,110 +42,106 @@ import java.util.TimeZone;
 
 public final class JfrGenerator {
 
-    private JfrGenerator() {}
+	private JfrGenerator() {
+	}
 
-    public static SourceStage create() {
-        return new SourceStage();
-    }
+	public static SourceStage create() {
+		return new SourceStage();
+	}
 
-    public static final class SourceStage {
-        private SourceStage() {}
+	public static final class SourceStage {
+		private SourceStage() {
+		}
 
-        /**
-         * Provide source code as a string for a valid <a href="https://openjdk.org/jeps/330">JEP 330</a>
-         * single-file source-code program, which will be profiled using JFR.
-         */
-        public ConfigurationStage source(String source) {
-            return new ConfigurationStage(Objects.requireNonNull(source, "Source is required"));
-        }
-    }
+		/**
+		 * Provide source code as a string for a valid <a href="https://openjdk.org/jeps/330">JEP
+		 * 330</a> single-file source-code program, which will be profiled using JFR.
+		 */
+		public ConfigurationStage source(String source) {
+			return new ConfigurationStage(Objects.requireNonNull(source, "Source is required"));
+		}
+	}
 
-    public static final class ConfigurationStage {
-        private final String source;
-        private ConfigurationStage(String source) {
-            this.source = source;
-        }
+	public static final class ConfigurationStage {
+		private final String source;
 
-        /**
-         * Name of a default configuration, for example {@code default} and
-         * {@code profile} are included in most OpenJDK builds.
-         */
-        public ExecutionStage configurationName(String configuration) {
-            return new ExecutionStage(source, Objects.requireNonNull(configuration,  "Configuration is required"), null);
-        }
+		private ConfigurationStage(String source) {
+			this.source = source;
+		}
 
-        /**
-         * Contents of a JFR configuration file.
-         */
-        public ExecutionStage configuration(String configuration) {
-            return new ExecutionStage(source, null, Objects.requireNonNull(configuration,  "Configuration is required"));
-        }
-    }
+		/**
+		 * Name of a default configuration, for example {@code default} and {@code profile} are
+		 * included in most OpenJDK builds.
+		 */
+		public ExecutionStage configurationName(String configuration) {
+			return new ExecutionStage(source, Objects.requireNonNull(configuration, "Configuration is required"), null);
+		}
 
-    public static final class ExecutionStage {
+		/**
+		 * Contents of a JFR configuration file.
+		 */
+		public ExecutionStage configuration(String configuration) {
+			return new ExecutionStage(source, null, Objects.requireNonNull(configuration, "Configuration is required"));
+		}
+	}
 
-        private final String source;
-        private final String configurationName;
-        private final String configurationContents;
+	public static final class ExecutionStage {
 
-        private ExecutionStage(String source, String configurationName, String configurationContents) {
-            this.source = source;
-            this.configurationName = configurationName;
-            this.configurationContents = configurationContents;
-        }
+		private final String source;
+		private final String configurationName;
+		private final String configurationContents;
 
-        public void execute(RecordingConsumer consumer) throws IOException {
-            Objects.requireNonNull(consumer, "Consumer is required");
-            Path temp = Files.createTempDirectory("jfr-generator").toAbsolutePath();
-            Path sourcePath = temp.resolve("Source.java");
-            Path configPath = temp.resolve("custom-config.jfc");
-            Path jfrPath = temp.resolve("recording.jfr");
-            try {
-                Files.writeString(sourcePath, source, StandardCharsets.UTF_8);
-                if (configurationContents != null) {
-                    Files.writeString(configPath, configurationContents, StandardCharsets.UTF_8);
-                }
-                String osName = System.getProperty("os.name");
-                boolean isWindows = osName != null && osName.startsWith("Windows");
-                Path javaBinary = Paths.get(System.getProperty("java.home"))
-                        .toAbsolutePath()
-                        .resolve("bin")
-                        .resolve(isWindows ? "java.exe" : "java");
-                Process process = new ProcessBuilder()
-                        .directory(
-                                temp.toFile()).inheritIO().command(javaBinary.toString(),
-                                String.format("-XX:StartFlightRecording=settings=%s,filename=%s,dumponexit=true",
-                                        configurationName != null ? configurationName : configPath.toAbsolutePath(),
-                                        jfrPath),
-                                // Bound resource usage.
-                                // We may want to make the JVM args configurable later on.
-                                "-Xmx64m",
-                                "-Xms64m",
-                                // Match current JVM timezone
-                                "-Duser.timezone=" + TimeZone.getDefault().getID(),
-                                sourcePath.toString())
-                        .start();
-                int exitStatus = process.waitFor();
-                if (exitStatus != 0) {
-                    throw new RuntimeException("Process failed to exit successfully. Status: " + exitStatus);
-                }
-                if (!Files.isRegularFile(jfrPath)) {
-                    throw new RuntimeException("Failed to create a JFR");
-                }
-                consumer.acceptRecording(jfrPath);
-            } catch (InterruptedException e) {
-                throw new RuntimeException(e);
-            } finally {
-                Files.deleteIfExists(sourcePath);
-                Files.deleteIfExists(jfrPath);
-                Files.deleteIfExists(configPath);
-                Files.delete(temp);
-            }
-        }
-    }
+		private ExecutionStage(String source, String configurationName, String configurationContents) {
+			this.source = source;
+			this.configurationName = configurationName;
+			this.configurationContents = configurationContents;
+		}
 
-    @FunctionalInterface
-    public interface RecordingConsumer {
-        void acceptRecording(Path recording) throws IOException;
-    }
+		public void execute(RecordingConsumer consumer) throws IOException {
+			Objects.requireNonNull(consumer, "Consumer is required");
+			Path temp = Files.createTempDirectory("jfr-generator").toAbsolutePath();
+			Path sourcePath = temp.resolve("Source.java");
+			Path configPath = temp.resolve("custom-config.jfc");
+			Path jfrPath = temp.resolve("recording.jfr");
+			try {
+				Files.writeString(sourcePath, source, StandardCharsets.UTF_8);
+				if (configurationContents != null) {
+					Files.writeString(configPath, configurationContents, StandardCharsets.UTF_8);
+				}
+				String osName = System.getProperty("os.name");
+				boolean isWindows = osName != null && osName.startsWith("Windows");
+				Path javaBinary = Paths.get(System.getProperty("java.home")).toAbsolutePath().resolve("bin")
+						.resolve(isWindows ? "java.exe" : "java");
+				Process process = new ProcessBuilder().directory(temp.toFile()).inheritIO().command(
+						javaBinary.toString(),
+						String.format("-XX:StartFlightRecording=settings=%s,filename=%s,dumponexit=true",
+								configurationName != null ? configurationName : configPath.toAbsolutePath(), jfrPath),
+						// Bound resource usage.
+						// We may want to make the JVM args configurable later on.
+						"-Xmx64m", "-Xms64m",
+						// Match current JVM timezone
+						"-Duser.timezone=" + TimeZone.getDefault().getID(), sourcePath.toString()).start();
+				int exitStatus = process.waitFor();
+				if (exitStatus != 0) {
+					throw new RuntimeException("Process failed to exit successfully. Status: " + exitStatus);
+				}
+				if (!Files.isRegularFile(jfrPath)) {
+					throw new RuntimeException("Failed to create a JFR");
+				}
+				consumer.acceptRecording(jfrPath);
+			} catch (InterruptedException e) {
+				throw new RuntimeException(e);
+			} finally {
+				Files.deleteIfExists(sourcePath);
+				Files.deleteIfExists(jfrPath);
+				Files.deleteIfExists(configPath);
+				Files.delete(temp);
+			}
+		}
+	}
+
+	@FunctionalInterface
+	public interface RecordingConsumer {
+		void acceptRecording(Path recording) throws IOException;
+	}
 }
