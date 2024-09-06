0110c0ff700251390a7b3b3974c12b6f0f5c3447
thegreystone <marcus@hirt.se>
2024-09-06 23:33:18 +0200

    Adding comment

--- core/org.openjdk.jmc.flightrecorder/src/main/java/org/openjdk/jmc/flightrecorder/jdk/JdkAttributes.java
+++ core/org.openjdk.jmc.flightrecorder/src/main/java/org/openjdk/jmc/flightrecorder/jdk/JdkAttributes.java
@@ -1398,6 +1398,7 @@ public final class JdkAttributes {
 			Messages.getString(Messages.ATTR_AGENT_INITIALIZATION_DURATION),
 			Messages.getString(Messages.ATTR_AGENT_INITIALIZATION_DURATION_DESC), TIMESPAN);
 
+	// Don't want the canonicalized size attribute, so creating a new one...
 	public static final IAttribute<IQuantity> RSS_SIZE = new Attribute<IQuantity>("size", //$NON-NLS-1$
 			Messages.getString(Messages.ATTR_RSS_SIZE), Messages.getString(Messages.ATTR_RSS_SIZE_DESC), MEMORY) {
 	};
