384af0cc328d29d8567d67595157fd2fc1e2081d
Martin Skarsaune <martin@skarsaune.net>
2026-01-25 10:47:49 +0100

    JMC-8237: Fix spotless

--- application/org.openjdk.jmc.kubernetes/src/main/java/org/openjdk/jmc/kubernetes/KubernetesDiscoveryListener.java
+++ application/org.openjdk.jmc.kubernetes/src/main/java/org/openjdk/jmc/kubernetes/KubernetesDiscoveryListener.java
@@ -117,7 +117,7 @@ public class KubernetesDiscoveryListener extends AbstractCachedDescriptorProvide
 		final String path = Utils.getSystemPropertyOrEnvVar(Config.KUBERNETES_KUBECONFIG_FILE,
 				new File(System.getProperty("user.home"), ".kube" + File.separator + "config").toString()); //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$
 		File configPath = new File(path);
-        // the YAML parsing is soo incredibly sloow, hence cache context names for later runs
+		// the YAML parsing is soo incredibly sloow, hence cache context names for later runs
 		if (contexts != null && contextsCached > configPath.lastModified()) {
 			return contexts;
 		}
